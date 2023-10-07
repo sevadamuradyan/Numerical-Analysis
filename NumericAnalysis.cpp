da5fde8b0f70cf2ef6d4978e067ffe0ed6f462e8
Սեւադա <93379078+sevadamuradyan@users.noreply.github.com>
2023-10-07 23:19:43 +0400

    Create NumericAnalysis.cpp

---
+++ NumericAnalysis.cpp
@@ -0,0 +1,276 @@
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+#include <conio.h>
+#include <string>
+double polynomial(double x)
+{
+    //return (x - 6) * (x - 5) * (x - 4) * (x - 3) * (x - 2) * (x - 1);
+    //return x * x * x - x + 1;
+    //return x * x * x * x - 2 * x * x * x + 4 * x - 1;
+    // return pow(x,4) + 3*pow(x,3) - 2*x;
+    //return 6 * pow(x, 5) - 16*pow(x, 3)  - 3;
+    /*Simple Iteration*/ //return x - ((x * x * x + 3 * x * x - 7) / 13);
+    /*Newton*/// return (x * x * x + 3 * x * x - 7) / (3 * x * x + 6 * x);
+    //double y = x - 0.01;
+    //return (x * x * x + 3 * x * x - 7) / ((x * x * x + 3 * x * x - 7) - (x * x * x + 3 * y * y - 7));
+
+}
+struct BisectionMethod{  
+    double Method(double left, double right, double epsilion)
+    {
+        double leftValue = polynomial(left);
+        double rightValue = polynomial(right);
+        double mid = left+abs(left-right) / 2;
+        double midValue = polynomial(mid);
+        while ( abs(midValue) > epsilion) {
+            if ((leftValue * midValue) < 0)
+                right = mid;
+            else
+                left = mid;
+
+            mid = left + abs(left - right) / 2;
+            leftValue = polynomial(left);
+            rightValue = polynomial(right);
+            midValue = polynomial(mid);
+        }
+
+        return mid;
+    }
+};
+
+struct SimpleIteration{
+//g(x) = x - (f(x)/Alpha), |Alpha| > 1/2(max[a,b]|f'(x)|) 
+    double Iteration(double xk_1, double epsilion)
+    {
+        double xk = polynomial(xk_1);
+        while (abs(xk - xk_1) > epsilion)
+        {
+            xk_1 = xk;
+            xk = polynomial(xk);
+        }
+        return xk;
+    }
+};
+
+struct NewtonMethod{
+private:
+    double f2Dif(double x)
+    {
+        return 6 * x + 6;
+    }
+    double FFunc(double x)
+    {
+        return x * x * x + 3 * x * x - 7;
+    }
+public:
+    double Method(double a, double b,double epsilion)
+    {
+        double xk_1 = a;
+        double xk = 99;
+        if (f2Dif(a) * FFunc(a)> 0)
+        {
+            xk_1 = a;
+        }
+        else
+            xk_1 = b;
+        while (abs(FFunc(xk)) > epsilion)
+        {
+            xk = xk_1 - polynomial(xk_1);
+            xk_1 = xk;
+        }
+        return xk;
+
+          
+    }
+};
+
+struct IntersectionMethod{
+private:
+    double f2Dif(double x)
+    {
+        return 6 * x + 6;
+    }
+    double FFunc(double x)
+    {
+        return x * x * x + 3 * x * x - 7;
+    }
+public:
+    double method(double a, double b, double epsilion)
+    {
+        double xk = 0;
+        double xk_1 = 199;
+        double xk_2 = 299;
+        if (f2Dif(a) * FFunc(a) > 0)
+        {
+            xk_1 = a;
+        }
+        else
+            xk_1 = b;
+        if (xk_1 == a)
+        {
+            xk_2 = a + 0.001;
+        }
+        else xk_2 = b - 0.001;
+
+            while (abs(FFunc(xk)) > epsilion)
+            {
+                xk = xk_1 - ((xk_1 * xk_1 * xk_1 + 3 *xk_1 * xk_1 - 7) / ((xk_1 * xk_1 * xk_1 + 3 * xk_1 * xk_1 - 7) - (xk_2 * xk_2 * xk_2 + 3 * xk_2 * xk_2 - 7))*(xk_1-xk_2));
+                xk_2 = xk_1;
+                xk_1 = xk;
+            }
+        return xk;
+    }
+
+};
+
+struct Gauss {
+    std::vector<double> GaussMethod(std::vector<std::vector<double>> A, std::vector<double> b)
+    {
+        std::vector<double> X(A.size(), 1);
+
+        for (int i = 0; i < A.size(); ++i)
+        {
+            for (int j = i + 1; j < A.size(); ++j)
+            {
+                if (A[i][i] == 0) throw "Sorry I can't solve your problem";
+                double factor = A[j][i] / A[i][i];
+                for (int k = 0; k < A[0].size(); ++k)
+                {
+                    A[j][k] -= factor * A[i][k];
+                }
+                b[j] -= factor * b[i];
+
+            }
+        }
+
+
+        for (int i = A.size() - 1; i >= 0; --i)
+        {
+            double sum = 0;
+            for (int j = i + 1; j < A[0].size(); ++j)
+            {
+                sum += A[i][j] * X[j];
+            }
+            if (A[i][i] == 0) throw "Sorry I can't solve your problem";
+            X[i] = (b[i] - sum) / A[i][i];
+        }
+        return X;
+
+    }
+};
+
+struct LU
+{
+    void croutsMethod(std::vector<std::vector<double>>& A, std::vector<std::vector<double>>& L, std::vector<std::vector<double>>& U) {
+        int n = A.size();
+
+        for (int i = 0; i < n; ++i) {
+            U[i][i] = 1;
+
+            for (int j = i; j < n; ++j) {
+                double sum = 0;
+
+                for (int k = 0; k < i; ++k) {
+                    sum += L[i][k] * U[k][j];
+                }
+
+                U[i][j] = A[i][j] - sum;
+            }
+
+            for (int j = i; j < n; ++j) {
+                if (i == j) {
+                    L[i][j] = 1;
+                }
+                else {
+                    double sum = 0;
+
+                    for (int k = 0; k < i; ++k) {
+                        sum += L[j][k] * U[k][i];
+                    }
+
+                    L[j][i] = (A[j][i] - sum) / U[i][i];
+                }
+            }
+        }
+    }
+
+    void solveSystem(std::vector<std::vector<double>>& L, std::vector<std::vector<double>>& U, std::vector<double>& B, std::vector<double>& X) {
+        int n = L.size();
+
+        std::vector<double> Y(n, 0);
+
+        // Solve LY = B
+        for (int i = 0; i < n; ++i) {
+            double sum = 0;
+            for (int j = 0; j < i; ++j) {
+                sum += L[i][j] * Y[j];
+            }
+            Y[i] = (B[i] - sum) / L[i][i];
+        }
+
+        // Solve UX = Y 
+        for (int i = n - 1; i >= 0; --i) {
+            double sum = 0;
+            for (int j = i + 1; j < n; ++j) { sum += U[i][j] * X[j]; }
+            X[i] = (Y[i] - sum) / U[i][i];
+        }
+    }
+};
+
+int main()
+{
+	std::vector<std::vector<double>> a ={{-2, 3, 6},{4, -1, 3},{1, 7, -1}};
+	std::vector<double> b = { 5, -5,6};
+
+    // Gauss
+    Gauss G;
+	std::vector<double> Y = G.GaussMethod(a, b);
+    std::cout << "\nSolution with Gauss: ";
+	for (const auto& x : Y)
+	{
+		if (x == -0) std::cout << abs(x) <<" ";
+		else std::cout << x <<" ";
+	}
+    
+    // LU
+    LU lu;
+    int n = a.size();
+    std::vector<std::vector<double>> L(n, std::vector<double>(n, 0));
+    std::vector<std::vector<double>> U(n, std::vector<double>(n, 0));
+    std::vector<double> X(n, 0);
+    lu.croutsMethod(a, L, U);
+    lu.solveSystem(L, U, b, X);
+    std::cout << "\nSolution With LU: ";
+    for (const auto& x : X)
+    {
+        if (x == -0) std::cout << abs(x) << " ";
+        else std::cout << x << " ";
+    }
+    
+    //Bisection
+    //BisectionMethod BisecMethod;
+    //std::cout << "\nSolution Bisection Method For Polynominal: " << BisecMethod.Method(-10  , -2.14, 0.01);
+    //std::cout << "\nSolution Bisection Method For Polynominal: " << BisecMethod.Method(-2.14, -0.54, 0.01);
+    //std::cout << "\nSolution Bisection Method For Polynominal: " << BisecMethod.Method(-0.54,  0.43, 0.01);
+    //std::cout << "\nSolution Bisection Method For Polynominal: " << BisecMethod.Method(0.43 ,     5, 0.01);
+
+    //SimpleIteration 
+    //SimpleIteration SimpleIter;
+    //std::cout << "\nSolution Simple Iteration: " << SimpleIter.Iteration(1, 0.001);
+
+    //Newton Method
+   // NewtonMethod NewtonMeth;
+    //std::cout <<"\nSolution Newton method for Polynominal: "<<NewtonMeth.Method(1, 2, 0.001);
+
+    //Intersection Method
+    //IntersectionMethod IntersectMeth;
+    //std::cout <<"\nSolution Intersection method for Polynominal: "<<IntersectMeth.method(1, 2, 0.01);
+
+
+}
+
+
+
+
